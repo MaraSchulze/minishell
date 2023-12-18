ca7f29bdbcf8d16b706cbdc24ffd3b2c0ab7cce7
fbock <fbock@student.42heilbronn.de>
2023-12-18 15:11:39 +0100

    alias commit

---
+++ src/builtins/echo.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 14:58:11 by fbock             #+#    #+#             */
+/*   Updated: 2023/12/18 15:11:36 by fbock            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+// takes the string to print after the echo command, or after -n if existing
+// if -n exists, newline is false.
+bool	echo(const char *after_echo, bool n_flag)
+{
+	if (!n_flag)
+		printf("%s\n", after_echo);
+	else
+		printf("%s", after_echo);
+}
