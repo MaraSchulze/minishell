ae464ab80dcd0825e6feadf2507d36c692432063
fbock <fbock@student.42heilbronn.de>
2023-12-18 14:55:18 +0100

    alias commit

--- src/parsing/parsing_main.c
+++ src/parsing/parsing_main.c
@@ -6,18 +6,21 @@
 /*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 13:17:21 by fbock             #+#    #+#             */
-/*   Updated: 2023/12/18 14:47:53 by fbock            ###   ########.fr       */
+/*   Updated: 2023/12/18 14:55:00 by fbock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	parse_line(char *entered_line)
+bool	parse_line(char *entered_line, t_pipe parsed_command)
 {
 	pipe_struct	parsed_args;
 	char		**tokens;
 
 	tokens = ft_split(entered_line, ' ');
 
-
+	while (*tokens)
+	{
+		return (true);
+	}
 }
