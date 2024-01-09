d8955171441ca76ab779ee2b6149919e9d57d1c8
fbock <fbock@student.42heilbronn.de>
2024-01-09 13:04:00 +0100

    alias commit

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: fbock <fbock@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:28:44 by fbock             #+#    #+#             */
-/*   Updated: 2024/01/09 10:13:17 by fbock            ###   ########.fr       */
+/*   Updated: 2024/01/09 12:24:13 by fbock            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ int	main(int argc, char **argv, char **envp)
 		// parsing the input for execution
 		if (!parsing_main(entered_line, &task))
 		{
-			printf("parsing dead\n");
+			// printf("parsing dead\n");
 			free(entered_line);
 			continue ;
 		}
